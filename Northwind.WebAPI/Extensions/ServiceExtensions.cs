f1dd0bc0454bdd518ebcfb13d52c834a1bc16da0
widiamalia <widiamalia36@gmail.com>
2022-10-22 21:30:31 +0700

    Web API Day01

---
+++ Northwind.WebAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Northwind.Domain.Base;
+using Northwind.Persistence;
+using Northwind.Persistence.Base;
+using Northwind.Services;
+using Northwind.Services.Abstraction;
+
+namespace Northwind.WebAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //Cors = Cross Oeigin resource Sharing
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+        // add IIS configure options deploy to IIS
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        //create a service once per request
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+
+        //configure to db
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<NorthwindContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("development")
+            ));
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+    }
+}
---
+++ Northwind.WebAPI/Mapping/MappingProfile.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Northwind.Contracts.Dto.Authentication;
+using Northwind.Contracts.Dto.Category;
+using Northwind.Contracts.Dto.Order;
+using Northwind.Contracts.Dto.OrderDetail;
+using Northwind.Contracts.Dto.Product;
+using Northwind.Contracts.Dto.Shipper;
+using Northwind.Contracts.Dto.Supplier;
+using Northwind.Domain.Models;
+
+namespace Northwind.WebAPI.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryForCreateDto>().ReverseMap();
+
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Product, ProductForCreateDto>().ReverseMap();
+            CreateMap<Product, ProductPhotoGroupDto>().ReverseMap();
+
+            CreateMap<ProductPhoto, ProductPhotoDto>().ReverseMap();
+            CreateMap<ProductPhoto, ProductPhotoCreateDto>().ReverseMap();
+            CreateMap<ProductPhoto, ProductPhotoGroupDto>().ReverseMap();
+
+            CreateMap<Supplier, SupplierDto>().ReverseMap();
+            CreateMap<Supplier, SupplierForCreateDto>().ReverseMap();
+
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Order, OrderForCreateDto>().ReverseMap();
+
+            CreateMap<OrderDetail, OrderDetailDto>().ReverseMap();
+            CreateMap<OrderDetail, OrderDetailForCreateDto>().ReverseMap();
+
+            CreateMap<Shipper, ShipperDto>().ReverseMap();
+
+            CreateMap<UserRegistrationDto, User>()
+                .ForMember(u => u.UserName, opt => opt.MapFrom(x => x.Email));
+
+            CreateMap<UserLoginDto, User>().ReverseMap();
+
+        }
+    }
+}
